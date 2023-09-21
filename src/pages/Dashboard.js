2417e68a2de6dc33b2a325c624cdf2be4833b28b
arjunivor <arjunkukreja330@gmail.com>
2023-09-21 18:29:31 +0530

    Added styling and routing

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,15 @@
+import TicketCard from '../components/TicketCard'
+
 const Dashboard = () => {
     return (
-        <div>Dashboard</div>
+        <div className="dashboard">
+            <h1>My Projects</h1>
+            <div className="ticket-container">
+                <TicketCard/>
+
+            </div>
+
+        </div>
     )
 }
 export default Dashboard
