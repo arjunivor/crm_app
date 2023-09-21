00faa6d211138f27d2e3329e636a9665feb4262c
arjunivor <arjunkukreja330@gmail.com>
2023-09-21 17:54:22 +0530

    Added some code to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Dashboard from './pages/Dashboard'
 const App = () => {
   return (
     <div className="app">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Dashboard/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
