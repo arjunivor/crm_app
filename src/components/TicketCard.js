a3b26130f2e07a4fb7f5329b500cee4603239671
arjunivor <arjunkukreja330@gmail.com>
2023-09-22 17:10:39 +0530

    added imports and code to TicketCard.js

--- src/components/TicketCard.js
+++ src/components/TicketCard.js
@@ -1,4 +1,23 @@
+import { Link, Link } from "react-router-dom";
+import AvatarDisplay from "./AvatarDisplay";
+import DeleteBlock from "./DeleteBlock";
+import PriorityDisplay from "./PriorityDisplay";
+import ProgressDisplay from "./ProgressDisplay";
+import StatusDisplay from "./StatusDisplay";
+
 const TicketCard = () => {
-  return <div>TicketCard</div>;
+  return (
+    <div className="ticket-card">
+      <div className="ticket-color"></div>
+      <Link>
+        <h3>title</h3>
+        <AvatarDisplay />
+        <StatusDisplay />
+        <PriorityDisplay />
+        <ProgressDisplay />
+      </Link>
+      <DeleteBlock />
+    </div>
+  );
 };
 export default TicketCard;
