f2a75a15f2441e6a77c5068162af52f47635518d
arjunivor <arjunkukreja330@gmail.com>
2023-09-25 22:49:11 +0530

    added ticket add code

--- src/pages/TicketPage.js
+++ src/pages/TicketPage.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
+
 const TicketPage = () => {
+  const [formData, setFormData] = useState({
+    status: "not started",
+    progress: 0,
+    timestamp: new Date().toISOString(),
+  });
   const editMode = false;
 
   const handleSubmit = () => {
     console.log("submitted");
   };
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  console.log(formData);
 
   return (
     <div className="ticket">
@@ -11,7 +29,24 @@ const TicketPage = () => {
       <div className="ticket-container">
         <form onSubmit={handleSubmit}>
           <section>
-            <label></label>
+            <label htmlFor="title">Title</label>
+            <input
+              id="title"
+              name="title"
+              type="text"
+              onChange={handleChange}
+              required={true}
+              value={formData.title}
+            />
+            <label htmlFor="description">description</label>
+            <input
+              id="description"
+              name="description"
+              type="text"
+              onChange={handleChange}
+              required={true}
+              value={formData.title}
+            />
           </section>
         </form>
       </div>
