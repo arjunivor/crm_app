756a2847375db5c71798725bd05629cd258d5472
arjunivor <arjunkukreja330@gmail.com>
2023-09-25 21:30:25 +0530

    updates

--- src/pages/TicketPage.js
+++ src/pages/TicketPage.js
@@ -1,4 +1,21 @@
 const TicketPage = () => {
-  return <div>TicketPage</div>;
+  const editMode = false;
+
+  const handleSubmit = () => {
+    console.log("submitted");
+  };
+
+  return (
+    <div className="ticket">
+      <h1>{editMode ? "Update your Ticket" : "Create a Ticket"}</h1>
+      <div className="ticket-container">
+        <form onSubmit={handleSubmit}>
+          <section>
+            <label></label>
+          </section>
+        </form>
+      </div>
+    </div>
+  );
 };
 export default TicketPage;
