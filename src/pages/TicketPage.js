0cb33f560009bb0fbe144bcbe1e5e64e2b0e44e5
arjunivor <arjunkukreja330@gmail.com>
2023-09-27 19:10:49 +0530

    added ability to make tickets (still WIP)

--- src/pages/TicketPage.js
+++ src/pages/TicketPage.js
@@ -120,6 +120,72 @@ const TicketPage = () => {
               />
               <label htmlFor="priority-5">5</label>
             </div>
+            {editMode && (
+              <>
+                <input
+                  type="range"
+                  id="progress"
+                  name="progress"
+                  value={formData.progress}
+                  min="0"
+                  max="100"
+                  onChange={handleChange}
+                />
+                <label htmlFor="progress">Progress</label>
+              </>
+            )}
+
+            <label>Status</label>
+            <select
+              name="status"
+              value={formData.status}
+              onChange={handleChange}
+            >
+              <option selected={formData.status === "done"} value="done">
+                Done
+              </option>
+              <option
+                selected={formData.status === "working on it"}
+                value="working on it"
+              >
+                working on it
+              </option>
+              <option selected={formData.status === "stuck"} value="stuck">
+                Stuck
+              </option>
+              <option
+                selected={formData.status === "not started"}
+                value="not started"
+              >
+                Not Started
+              </option>
+            </select>
+            <input type="submit" />
+          </section>
+          <section>
+            <label htmlFor="owner">Owner</label>
+            <input
+              id="owner"
+              name="owner"
+              type="text"
+              onChange={handleChange}
+              required={true}
+              value={formData.owner}
+            />
+            <label htmlFor="avatar">Avatar</label>
+            <input
+              id="avatar"
+              name="avatar"
+              type="url"
+              onChange={handleChange}
+              required={true}
+              value={formData.avatar}
+            />
+            <div className="img-preview">
+              {formData.avatar && (
+                <img src={formData.avatar} alt="image preview" />
+              )}
+            </div>
           </section>
         </form>
       </div>
